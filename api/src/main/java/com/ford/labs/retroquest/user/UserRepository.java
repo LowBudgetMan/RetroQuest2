6af374cecfeaf8d1bca2007f518b520a40a5d119
Nick Reuter <thenewimagineer@gmail.com>
2023-11-15 00:14:08 -0500

    Remove unused User code

--- api/src/main/java/com/ford/labs/retroquest/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ford.labs.retroquest.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
